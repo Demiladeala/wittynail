ba3c90ccd54b983e7e1e3544007a9219c9a2965c
Oluwademilade Ala <demiladeala@gmail.com>
2026-03-18 23:16:19 +0100

    fix: add www to backend api routes

--- src/context/Product.jsx
+++ src/context/Product.jsx
@@ -4,35 +4,35 @@ import { fetchApi } from "../apis/Index";
 export const ProductContextProvider = createContext(null);
 
 const Product = ({ children }) => {
-	const [data, setData] = useState(null);
-	const [bannerData, setBannerData] = useState(null);
+  const [data, setData] = useState(null);
+  const [bannerData, setBannerData] = useState(null);
 
-	useEffect(() => {
-		const productApi = "https://wittynailtip.com/backend/product.php";
-		const bannerApi = "https://wittynailtip.com/backend/banners.php";
-		async function fetchData() {
-			try {
-				const result = await fetchApi(productApi);
-				const bannerResult = await fetchApi(bannerApi);
-				if (result.success && bannerResult.success) {
-					setData(result.data.data);
-					setBannerData(bannerResult.data.data);
-				} else {
-					console.log(result.data.message);
-					console.log(bannerResult.data.message);
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		}
-		fetchData();
-	}, []);
+  useEffect(() => {
+    const productApi = "https://www.wittynailtip.com/backend/product.php";
+    const bannerApi = "https://www.wittynailtip.com/backend/banners.php";
+    async function fetchData() {
+      try {
+        const result = await fetchApi(productApi);
+        const bannerResult = await fetchApi(bannerApi);
+        if (result.success && bannerResult.success) {
+          setData(result.data.data);
+          setBannerData(bannerResult.data.data);
+        } else {
+          console.log(result.data.message);
+          console.log(bannerResult.data.message);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
 
-	return (
-		<ProductContextProvider.Provider value={{ data, bannerData }}>
-			{children}
-		</ProductContextProvider.Provider>
-	);
+  return (
+    <ProductContextProvider.Provider value={{ data, bannerData }}>
+      {children}
+    </ProductContextProvider.Provider>
+  );
 };
 
 export default Product;
